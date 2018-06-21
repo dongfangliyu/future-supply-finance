994fcb97b51b11f6320383abdda2f982082f3810
quan100 <328053660@qq.com>
2018-06-21 15:01:01 +0800

    修改Message 对象中的属性 message

--- common-utils/base-common/src/main/java/cn/fintecher/common/utils/basecommon/message/Message.java
+++ common-utils/base-common/src/main/java/cn/fintecher/common/utils/basecommon/message/Message.java
@@ -3,15 +3,15 @@ package cn.fintecher.common.utils.basecommon.message;
 import lombok.Data;
 
 @Data
-public class Message {
+public class Message<T> {
 
     private Integer code = null;
 
     private String type = null;
 
-    private Object message = null;
+    private T message = null;
 
-    public Message(Integer code, String type, Object message) {
+    public Message(Integer code, String type, T message) {
         this.code = code;
         this.type = type;
         this.message = message;
