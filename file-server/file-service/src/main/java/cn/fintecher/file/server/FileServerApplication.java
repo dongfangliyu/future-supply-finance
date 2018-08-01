18a10592411ceeb95afdcf689d074290165a8a51
wangquan <328053660@qq.com>
2018-08-01 17:10:43 +0800

    1、新增注册中心模块 2、修改其它服务，连接注册中心

--- file-server/file-service/src/main/java/cn/fintecher/file/server/FileServerApplication.java
+++ file-server/file-service/src/main/java/cn/fintecher/file/server/FileServerApplication.java
@@ -2,8 +2,10 @@ package cn.fintecher.file.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
+@EnableEurekaClient
 @SpringBootApplication
 @EnableFeignClients
 public class FileServerApplication {
--- lock-server/lock-service/src/main/java/cn/fintecher/lock/lockservicer/LockServiceApplication.java
+++ lock-server/lock-service/src/main/java/cn/fintecher/lock/lockservicer/LockServiceApplication.java
@@ -2,10 +2,12 @@ package cn.fintecher.lock.lockservicer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by lijinlong on 2017/05/26.
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class LockServiceApplication {
 
